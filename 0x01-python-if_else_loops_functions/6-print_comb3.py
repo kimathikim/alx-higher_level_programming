4c45e763a2eaa47b1fae5d2631bddf777095949a
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-21 02:12:56 +0300

    posible combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(48, 58):
     for j in range(48, 58):
         if j > i and i < 56:
-            print("{}{}".format(chr(i),chr(j)), end=", ")
+            print("{}{}".format(chr(i), chr(j)), end=", ")
 print("{}".format(89))
