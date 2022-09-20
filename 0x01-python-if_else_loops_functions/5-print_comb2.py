29261398164590e3873fbe18424dc2c4eda5e967
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-21 01:39:57 +0300

    00 - 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if (i != 99):
-       print("{}{}, ".format(int(i / 10), (i % 10)), end="")
+        print("{}{}, ".format(int(i / 10), (i % 10)), end="")
     else:
         print("{}".format(i))
