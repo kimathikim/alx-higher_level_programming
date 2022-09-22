2dde2832d5a98499810c237cc05633c7976006ef
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-22 19:36:23 +0300

    import *

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
-"""
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, div, mul
-    if (len(sys.argv) != 4):
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+    from sys import argv
+    from calculator_1 import *
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if sys.argv[2] == '+':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-"""
-from sys import argv
-from calculator_1 import add, div, mul, sub
-
-
-def main():
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
@@ -42,7 +19,3 @@ def main():
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-
-if __name__ == "__main__":
-    main()
