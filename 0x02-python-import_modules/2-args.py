11b02993b0576f8ec5093949c8bf9f02ac664c57
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-22 15:19:48 +0300

    commandline argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     if (len(argv) == 1):
+        print("{} arguments.".format((len(argv) - 1)))
+    elif (len(argv) == 2):
         print("{} argument.".format((len(argv) - 1)))
     else:
-        print("{} argument:".format((len(argv) - 1)))
+        print("{} arguments:".format((len(argv) - 1)))
     for i in range(len(argv)):
         if (i > 0):
             print("{}: {}".format((i), (argv[i])))
