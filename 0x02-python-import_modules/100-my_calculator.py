fcd7beaed5ef6f9bcfe86bcfd9e6db001c32c0ae
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-22 19:07:47 +0300

    import *

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    from calculator_1 import *
+import sys
+from calculator_1 import *
+
+def main():
     result = 0
     if (len(sys.argv) != 3):
         print("./100-my_calculator.py a operator b")
@@ -12,16 +13,19 @@ if __name__ == "__main__":
     b = int(sys.argv[3])
     if sign == '+':
         result = add(a, b)
-        print("{} + {} = {}".format(a, b, result))
+        print("{} {} {} = {}".format(a, argv, b, result))
     elif sign == '-':
         result = sub(a, b)
-        print("{} - {} = {}".format(a, b, result))
+        print("{} {} {} = {}".format(a, argv, b, result))
     elif sign == '*':
         result = mul(a, b)
-        print("{} * {} = {}".format(a, b, result))
+        print("{} {} {} = {}".format(a, argv, b, result))
     elif sign == '/':
         result = div(a, b)
-        print("{} / {} = {}".format(a, b, result))
+        print("{} {} {} = {}".format(a, argv, b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
+if __name__ == "__main__":
+    main()
