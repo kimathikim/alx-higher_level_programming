852b14f6c751c50c7e661f806a58a62f447a123c
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-22 19:09:09 +0300

    import *

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,6 +2,7 @@
 import sys
 from calculator_1 import *
 
+
 def main():
     result = 0
     if (len(sys.argv) != 3):
@@ -13,19 +14,20 @@ def main():
     b = int(sys.argv[3])
     if sign == '+':
         result = add(a, b)
-        print("{} {} {} = {}".format(a, argv, b, result))
+        print("{} {} {} = {}".format(a, argv[2], b, result))
     elif sign == '-':
         result = sub(a, b)
-        print("{} {} {} = {}".format(a, argv, b, result))
+        print("{} {} {} = {}".format(a, argv[2], b, result))
     elif sign == '*':
         result = mul(a, b)
-        print("{} {} {} = {}".format(a, argv, b, result))
+        print("{} {} {} = {}".format(a, argv[2], b, result))
     elif sign == '/':
         result = div(a, b)
-        print("{} {} {} = {}".format(a, argv, b, result))
+        print("{} {} {} = {}".format(a, argv[2], b, result))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
+
 
 if __name__ == "__main__":
     main()
