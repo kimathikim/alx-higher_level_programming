f011da290dca06c4a6684abe33b688417cc071bf
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-24 20:18:35 +0300

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx < 0 or idx > length - 1:
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
