f7160307f2d449782e46a38fd8dc0d6adee2048d
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-24 20:01:11 +0300

    value at index x

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    length = len(my_list)
+    for i in range(length):
+        if idx < 0 or idx > length:
+            return
+        if i == idx:
+            return (my_list[i])
