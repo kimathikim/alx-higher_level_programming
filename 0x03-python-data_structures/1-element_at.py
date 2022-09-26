8e0241579c7fc715e9874c78900235de473be68d
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-27 01:24:33 +0300

    copy

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     length = len(my_list)
     for i in range(length):
         if idx < 0 or idx > length -1:
-            return
+            return ("None")
         if i == idx:
             return (my_list[i])
