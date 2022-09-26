f14be52a7507f8909c14a0d61005662818e3b6c9
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-26 23:18:15 +0300

    return tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     s = sentence
+    t = ()
     if len(s) == 0:
-        return (len(s), "None")
-    return (len(s), s[0])
+        t += len(s),
+        t += "None",
+    else:
+        t += len(s),
+        t += s[0],
+    return (t)
+    
