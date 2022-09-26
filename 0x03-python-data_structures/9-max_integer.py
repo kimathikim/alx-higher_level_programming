f2120a99f38647999b800f2ece7a412f2be37f57
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-27 00:00:31 +0300

    Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
     for i in range(len(my_list)):
         if (my_list[i - 1] > my_list[i]):
             temp = my_list[i - 1]
