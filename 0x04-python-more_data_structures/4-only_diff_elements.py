1e56e5612f194ccb23a1031e6dd7bdb701e4fa81
Brian Kimathi Muriuki <briankimathi94@gmail.com>
2022-09-29 03:16:18 +0300

    not in _2

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set3 = set_1 | set_2
+    set3 = set_1 ^ set_2
     return set3
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
