661dd3557644fe40f509819f432d822d1f47b4a7
kimathikim <briankimathi94@gmail.com>
2023-01-24 00:18:16 +0300

    Exeption

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    Qu = 0
+    try:
+        qu = a / b
+    except ZeroDivisionError:
+        qu = None
+   
+    finally:
+        print("Inside result:{}".format(qu))
+    
+    return qu
