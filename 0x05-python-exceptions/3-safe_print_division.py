b143efe7b73fbe86abc6ba47f757c3c90b6cd578
kimathikim <briankimathi94@gmail.com>
2023-01-25 08:40:05 +0300

    ZeroDevError

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         qu = None
     finally:
-        print("Inside result:{}".format(qu))
+        print("Inside result: {}".format(qu))
     return qu
