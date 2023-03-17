06ea276c87923ff860d408e7613254557b12a955
kimathikim <briankimathi94@gmail.com>
2023-03-17 20:49:23 +0300

    where

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,5 +3,6 @@
 SELECT t1.name
 FROM tv_genres t1
     INNER JOIN tv_show_genres t2 ON t1.id = t2.genre_id
-WHERE t2.show_id = 8
+    INNER JOIN tv_shows t3 ON t2.show_id = t3.id
+WHERE t3.title = "Dexter"
 ORDER BY t1.name;
