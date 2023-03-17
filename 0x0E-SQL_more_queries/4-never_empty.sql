8c4dad9b62423ba6cf0babc639b832ff7da9cbd3
kimathikim <briankimathi94@gmail.com>
2023-03-17 15:35:32 +0300

    create table

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- script that creates the table id_not_null
+-- script that creates the table force_name
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256) NOT NULL
+);
