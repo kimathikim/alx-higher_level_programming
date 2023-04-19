fa3627af7f75ceb20479104dafdda15430eca0b6
kimathikim <briankimathi94@gmail.com>
2023-04-19 13:49:49 +0300

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
-
 import MySQLdb
 from sys import argv
 
 
-def select_s():
-    """conect and query data from a database"""
-
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2],
-        db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
-
-    for i in rows:
-        print(i)
+def mysqlconnnect():
+    """
+    Connecting and quering data base
+    """
+    try:
+        db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                        user=argv[1], passwd=argv[2],
+                                        db=argv[3], charset="utf8")
+    except Exception:
+        print("can't connect to database")
+        return 0
+    cur = db_connection.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
-    db.close()
+    db_connection.close()
 
 
-select_s()
+mysqlconnnect()
