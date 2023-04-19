cf775e3dcf8d1c0843cc102ae782ebc65fdfb09f
kimathikim <briankimathi94@gmail.com>
2023-04-20 00:14:52 +0300

    add a not existing user

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for query"""
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+if __name__ == "__main__":
+    """
+    adds the State object “Louisiana” to the database hbtn_0e_6_usa
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    rows = session.query(State).all()
+
+    if not session.query(State).filter(State.name.in_(
+            ['Louisiana'])).all():
+        session.add(State(name='Louisiana'))
+
+    value = 0
+    for row in rows:
+        if row == rows[-1]:
+            print(row.id)
+    session.commit()
+    session.close()
