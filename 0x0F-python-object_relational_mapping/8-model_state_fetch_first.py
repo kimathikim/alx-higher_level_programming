1e10766f82ab6ac43dcce2c8256070107e14f5d0
kimathikim <briankimathi94@gmail.com>
2023-04-19 22:46:10 +0300

    quering one row with orm

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for query"""
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+import sys
+from sqlalchemy import create_engine
+if __name__ == "__main__":
+    """ prints the first State object from the database hbtn_0e_6_usa"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    rows = session.query(State).first()
+    print("{}: {}".format(rows.id, rows.name))
