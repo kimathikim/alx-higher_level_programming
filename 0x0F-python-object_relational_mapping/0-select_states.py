2a3f1959fce3946486f9447a1286eb174299f6c8
kimathikim <briankimathi94@gmail.com>
2023-04-19 13:52:39 +0300

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,13 +7,10 @@ def mysqlconnnect():
     """
     Connecting and quering data base
     """
-    try:
-        db_connection = MySQLdb.connect(host="localhost", port=3306,
-                                        user=argv[1], passwd=argv[2],
-                                        db=argv[3], charset="utf8")
-    except Exception:
-        print("can't connect to database")
-        return 0
+
+    db_connection = MySQLdb.connect(host="localhost", port=3306,
+                                    user=argv[1], passwd=argv[2],
+                                    db=argv[3], charset="utf8")
     cur = db_connection.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
