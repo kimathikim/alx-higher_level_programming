600cc664713d04d87c3e80a96aedf51daedb955f
kimathikim <briankimathi94@gmail.com>
2023-06-13 23:10:22 +0300

    if statement

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const Leng = process.argv;
-if (Leng.length === 2) {
+if (Leng.length == 2) {
   console.log('No arguments');
-} else if (Leng.length === 3) {
+} else if (Leng.length == 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
