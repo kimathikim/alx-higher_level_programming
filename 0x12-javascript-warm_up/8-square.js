5c809eaedc23d590c64dc7f332a3ea3f2294a4e0
kimathikim <briankimathi94@gmail.com>
2023-06-14 17:01:25 +0300

    Export mudule

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,15 +3,15 @@
 const Leng = process.argv;
 if (Number(Leng[2])) {
   const num = Number(Leng[2]);
-  for (let row = 0; row < num; row++) {
-    // defining a row
-    let sqr = '';
-    for (let col = 0; col < num; col++) {
-      // defining a column
-      sqr += 'X';
+  for (let row = 0;  row < num;  row++) {
+    //defining a row
+    let sqr = ''
+    for (let col= 0; col < num; col++) {
+        //defining a column
+        sqr += 'X';
     }
-    // printing square
-    console.log(sqr);
+    //printing square
+    console.log(sqr)
   }
 } else {
   console.log('Missing number of occurences');
