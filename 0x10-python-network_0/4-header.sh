b31bdcafd1619a68997bf1860733a313812e6f6d
kimathikim <briankimathi94@gmail.com>
2023-08-26 13:44:19 +0300

    set a header and send a get request

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#  bash script that receives GET method with attributtes and displays
-#  body
-#
-url="$1"
-curl -sH "X-School-User-Id: 98" "$url"
+#  bash script that receives GET method with attributtes and displays body
+curl -sH "X-School-User-Id: 98" "$1"
