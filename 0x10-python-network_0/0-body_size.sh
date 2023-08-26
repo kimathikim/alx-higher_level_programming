a31971d3c84be5471c6bbc10aca457a70ee2cc0e
kimathikim <briankimathi94@gmail.com>
2023-08-26 11:44:51 +0300

    print the response size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#  Bash script that takes in a URL,
-#  sends a request to that URL,
-#  and displays the size of the body of the response
-
-url="$1"
-curl -sI "$url" | grep -i Content-Length | awk '{print $2}'
+#  Bash script that takes in a URL send a request and display the size of the response
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
