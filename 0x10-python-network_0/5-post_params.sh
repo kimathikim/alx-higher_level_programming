024c412c447591717813b576dd87f8f07e0db6b4
kimathikim <briankimathi94@gmail.com>
2023-08-26 14:13:01 +0300

    make a post request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#  bash script that receives GET method with attributtes and displays
-#  body
-#
-url="$1"
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$url"
+#  bash script that receives GET method with attributtes and displays body
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
