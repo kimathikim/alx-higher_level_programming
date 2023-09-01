0447afdadfcd60f6771d37ccd622d8d356cbf92c
kimathikim <briankimathi94@gmail.com>
2023-09-01 14:25:36 +0300

    get the value of a header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Python script that takes in a URL, sends a request to the URL and
+displays the value of the variable X-Request-Id in the response header"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    heads = req.headers
+    for key, value in heads.items():
+        if key == 'X-Request-Id':
+            print('{}'.format(value))
