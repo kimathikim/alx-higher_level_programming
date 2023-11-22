ccac628485d5d221f8da43496017f79fe9d76b53
Brian Kimathi M <briankimathi94@gmail.com>
2023-11-22 05:48:22 +0300

    print every state starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,25 +5,30 @@ from sys import argv
 
 
 def state_id():
-    """t lists all states with a name starting with
-    N (upper N) from the database hbtn_0e_0_usa"""
+    """List all states with a name starting with N (upper N)
+    from the database hbtn_0e_0_usa"""
     try:
         db = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
         )
-    except Exception:
-        print("unable to connect to the database")
-        return 0
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(f"Unable to connect to the database: {e}")
+        return
 
-    for i in rows:
-        for b in i:
-            if type(b) == str and "N" == b[0]:
-                print(i)
-    cur.close()
-    db.close()
+    with db.cursor() as cur:
+        try:
+            cur.execute("SELECT * FROM states")
+            rows = cur.fetchall()
+            for state in rows:
+                if isinstance(state[1], str) and state[1].startswith("N"):
+                    print(state)
+        except MySQLdb.Error as e:
+            print(f"Error executing the query: {e}")
 
 
-state_id()
+if __name__ == "__main__":
+    state_id()
