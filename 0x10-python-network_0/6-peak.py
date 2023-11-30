9647e40712a1cdc54b5d43d0c6d68093af7f32b8
Brian Kimathi M <briankimathi94@gmail.com>
2023-12-01 02:40:07 +0300

    sort

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+# fuctuion to find peak in a unsorted list
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
