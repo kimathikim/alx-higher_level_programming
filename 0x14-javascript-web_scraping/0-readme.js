925c93a97a908c84359910f874a64e1115f5c578
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-03 18:11:58 +0300

    read file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,13 +4,12 @@
  * Read and print the contents of a file.
  */
 
-const fs = require('fs');
+const fs = require("fs");
 
 const filePath = process.argv[2];
-
 try {
-  const data = fs.readFileSync(filePath, 'utf-8');
+  const data = fs.readFileSync(filePath, "utf-8");
   console.log(data);
 } catch (err) {
-  console.log(err);
+  console.error(err);
 }
