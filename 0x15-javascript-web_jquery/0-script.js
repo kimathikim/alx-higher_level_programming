e5d805e7d12a0edebcd48856d2f2530f888a503d
Brian Kimathi M <briankimathi94@gmail.com>
2024-01-12 17:04:15 +0300

    styling using vanilla js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,2 @@
+// script that updates the text color of the HTML tag HEADER to red (#FF0000)
+document.querySelector("HEADER").style.color = "#FF0000";
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# alx-higher_level_programming
+0x15. JavaScript - Web jQuery
